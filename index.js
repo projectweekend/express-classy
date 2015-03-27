a2e7fce9f19c685ed8ca083466ce28f445fbb188
Brian Hines <brian@projectweekend.net>
2015-03-27 05:41:10 -0500

    Exports

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+exports.ClassyBase = require( "./lib/classy" );
 exports.CreateHandler = require( "./lib/create" );
 exports.ReadHandler = require( "./lib/read" );
+exports.UpdateHandler = require( "./lib/update" );
+exports.DeleteHandler = require( "./lib/delete" );
+
